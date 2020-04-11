0660d19abeae797619858a3aef1fb48621880e12
Ashok Dey <ashokdey100@gmail.com>
2020-04-11 23:03:15 +0530

    add: dummy typeDefs, resolvers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
-import version, { welcome } from './module';
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(version());
-console.log(welcome());
+// Define a schema
+const typeDefs = `
+  type Query {
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+`;
+
+// define resolvers
+const resolvers = {
+  Query: {
+    user() {
+      return {
+        id: 99,
+        name: 'Red Skull',
+        email: 'rs@skull.com',
+      };
+    },
+  },
+};
+
+// create the server
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+// start the server
+server.start(() => console.log('Server is up and running'));
--- src/module.js
+++
@@ -1,6 +0,0 @@
-export default function version() {
-  return '1.1.0';
-}
-export function welcome() {
-  return 'Hello GraphQL';
-}
