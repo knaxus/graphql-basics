e362202615d451ab508a01f776c792938af78b20
Ashok Dey <ashokdey100@gmail.com>
2020-04-12 13:14:37 +0530

    add: relation of users & posts

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
 
   type Post {
@@ -44,9 +45,16 @@ const resolvers = {
       return dummyData.posts.filter((post) => post.title.toLowerCase().includes(args.title.toLowerCase()));
     },
   },
+  // Let graphQL know about the relations
   Post: {
     author(parent, args, ctx, info) {
       return dummyData.users.find((user) => user.id === parent.author);
+    },
+  },
+
+  User: {
+    posts(parent, args, ctx, info) {
+      return dummyData.posts.filter((post) => post.author === parent.id);
     },
   },
 };
